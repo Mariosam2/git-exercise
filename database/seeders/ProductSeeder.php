e6f045271770d9705c66571b84fa0080a358b21d
MaTtiaa01 <mattia2001@live.it>
2023-01-12 16:11:00 +0100

    create db seeder close #2

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProductSeeder extends Seeder
 {
@@ -12,8 +14,14 @@ class ProductSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_product = new Product();
+            $new_product->name = $faker->word(2, true);
+            $new_product->description = $faker->text();
+            $new_product->price = $faker->randomFloat(2, 1, 100);
+            $new_product->save();
+        }
     }
 }
